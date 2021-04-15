3fa29dbe42c08f3f4f74247664322b06a9451801
arytio <tioagung34@gmail.com>
2021-04-15 19:19:52 +0800

    latihan 1

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Stateless Widget Hello World"),
+        ),
+        body: Center(child: Text("190030305_Agung_Ari_Prasetya")),
+      ),
+    );
+  }
+}
